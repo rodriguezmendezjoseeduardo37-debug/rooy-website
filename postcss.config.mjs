36073828cff00d4f73ba11b8de50c153d24e0f23
rodriguezmendezjoseeduardo37-debug <rodriguezmendezjoseeduardo37@gmail.com>
2026-01-19 17:14:00 -0600

    Fix: Configuración correcta de Tailwind v4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    // EN V4 SE USA ESTE PAQUETE, NO 'tailwindcss' A SECAS
+    '@tailwindcss/postcss': {}, 
   },
 };
 
